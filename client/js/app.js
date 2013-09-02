c63e36a350e9df95a7742fb49ac99024239e2a8f
manuchandraprasad <manuchandraprasadr@gmail.com>
2013-09-02 07:04:14 +0530

    Cleaned dir structure

---
+++ client/js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+
+var app = angular.module('myApp', [])
+
+
+app.run(function ($rootScope, $location, $anchorScroll, $routeParams) {
+
+    $rootScope.$on('$routeChangeSuccess', function (newRoute, oldRoute) {
+            $location.hash($routeParams.scrollTo);
+            $anchorScroll();
+        },
+        $rootScope.location = $location
+    );
+})
+;
+
+
+
+// Declare app level module which depends on filters, and services
+
+ app.config(['$routeProvider', function($routeProvider) {
+  	$routeProvider.when('/', {'templateUrl': 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/add/billm', {templateUrl: 'partials/billm.html', controller: 'BillmCtrl'});
+   	$routeProvider.when('/add/coupons', {templateUrl: 'partials/coupons.html', controller: 'CouponsCtrl'});
+   	$routeProvider.when('/add/delivery', {templateUrl: 'partials/delivery.html', controller: 'DeliveryCtrl'});
+   	$routeProvider.when('/add/format', {templateUrl: 'partials/format.html', controller: 'FormatCtrl'});
+   	$routeProvider.when('/add/frequency', {templateUrl: 'partials/frequency.html', controller: 'FrequencyCtrl'});
+   	$routeProvider.when('/add/hook', {templateUrl: 'partials/hook.html', controller: 'HookCtrl'});
+   	$routeProvider.when('/add/hooktemp', {templateUrl: 'partials/hooktemp.html', controller: 'HooktempCtrl'});
+   	$routeProvider.when('/add/pos_stmt', {templateUrl: 'partials/pos_stmt.html', controller: 'PositionCtrl'});
+   	$routeProvider.when('/add/sfp', {templateUrl: 'partials/sfp.html', controller: 'SfpCtrl'});
+    $routeProvider.when('/add/slogen', {templateUrl: 'partials/slogen.html', controller: 'SlogenCtrl'});
+    $routeProvider.when('/add/station', {templateUrl: 'partials/station.html', controller: 'StationCtrl'});
+    $routeProvider.when('/add/template', {templateUrl: 'partials/template.html', controller: 'TemplateCtrl'});
+    $routeProvider.when('/add/voice', {templateUrl: 'partials/voice.html', controller: 'VoiceCtrl'});
+
+
+    $routeProvider.otherwise({redirectTo: '/'});
+  }]);
---
+++ client/js/controllers.js
@@ -0,0 +1,112 @@
+'use strict';
+
+/* Controllers */
+
+
+function HomeCtrl($scope) {
+    $scope.name = 'ImageQuick';
+}
+function BillmCtrl($scope) {
+    $scope.name = 'BillmCtrl';
+}
+function CouponsCtrl($scope) {
+    $scope.name = 'Coupons';
+}
+function DeliveryCtrl($scope) {
+    $scope.name = 'Delivery';
+}
+function FormatCtrl($scope) {
+    $scope.name = 'Format';
+}
+function FrequencyCtrl($scope) {
+    $scope.name = 'Frequency';
+}
+function HookCtrl($scope) {
+    $scope.name = 'Hook';
+}
+function HooktempCtrl($scope) {
+    $scope.name = 'Hooktemp';
+}
+function PositionCtrl($scope) {
+    $scope.name = 'Position';
+}
+function SfpCtrl($scope) {
+    $scope.name = 'Sfp';
+}
+function SlogenCtrl($scope) {
+    $scope.name = 'Slogen';
+}
+function StationCtrl($scope) {
+    $scope.name = 'Station';
+}
+function TemplateCtrl($scope) {
+    $scope.name = 'Template';
+}
+function VoiceCtrl($scope) {
+    $scope.name = 'Voice';
+}
+
+
+
+
+
+
+
+/*
+angular.module('myApp.controllers', []).
+  controller('BillmCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('CouponsCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('DeliveryCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('FormatCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('FrequencyCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('HookCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('HooktempCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('PositionCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('SfpCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('SlogenCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  controller('StationCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+   controller('TemplateCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+    controller('VoiceCtrl', [function($scope) {
+  	$scope.home="ImageQuick";
+
+  }]).
+  
+  controller('HomeCtrl',[function($scope) {
+    $scope.name = 'Campsia';
+}]);*/
